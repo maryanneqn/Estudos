3e55eb3c159ae0522a9d79a5ea8a95baaa7b8202
maryanneqn <maryannequintao3210@gmail.com>
2025-08-25 19:44:50 -0300

    Organização das pastas

---
+++ Linguagem de programação/AULA 04/carro.py
@@ -0,0 +1,6 @@
+km = int (input ("Digite a quantidade de KM percorridos:"))
+d = float(input ("DIgite a quantidade de dias que o carro foi utilizado:"))
+
+p = (0.15* km) + (60*d)
+
+print ("O valor a pagar do aluguel do carro é:", p) 
