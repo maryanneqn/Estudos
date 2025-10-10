ce58d2250078e57da0ae6d482330149f577b8d88
maryanneqn <maryannequintao3210@gmail.com>
2025-10-09 22:18:48 -0300

    Desenvolvimento Web 09/10

---
+++ Desenvolvimento WEB/Aulas/semana_11/cadatro.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cadrastro PHP</title>
+</head>
+<body>
+
+    <p>Seus dados foram cadastrados com sucesso!</p>
+    
+</body>
+</html>
